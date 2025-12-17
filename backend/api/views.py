453ffccb8c2906eb9bb496e9e5c3e6d714ee4fae
Allan <allanhender@gmail.com>
2025-12-17 09:44:10 +0000

    cleaning up views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,3 @@
-from email.headerregistry import ParameterizedMIMEHeader
-from django.shortcuts import render
 from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import NoteSerializer, UserSerializer
